18de04f827cb16e5303ad65bf2349cbba6768141
Alexis Nguyen <alexisnguyen18@icloud.com>
2023-02-26 12:50:03 -0800

    v4

--- app/src/main/java/com/example/a15squares/Model.java
+++ app/src/main/java/com/example/a15squares/Model.java
@@ -1,26 +1,25 @@
 package com.example.a15squares;
 
 public class Model {
+    public int numSquares = 5; // Initializes the grid to 4 squares
+    public int seed = (numSquares-1) * (numSquares-1); // Grid size
+    public int [][] board = new int[numSquares-1][numSquares-1]; // Board values
 
-    //Grid size
-    public int numSquares = 3;
-    public int seed = (numSquares-1) * (numSquares-1);
-
-    //Board values
-    public int [][] board = new int[numSquares-1][numSquares-1];
-    public int [] solution = new int[seed];
-
-    //Test whether two squares were selected
+    // Test whether the two squares are valid selections
     public boolean isBlank = false;
     public boolean isNextTo = false;
-    public boolean resetTouched = true;
 
+    public boolean resetTouched = true; // Reset button pressed
+
+    // Location of blank square
     public double blankX = numSquares-2;
     public double blankY = numSquares-2;
 
+    // Location of other valid square selected
     public double squareX = 0;
     public double squareY = 0;
 
+    // Location of just touched
     public float touchX = 0;
     public float touchY = 0;
 
